d651803a822b113441520a6cd9008b4529a4775e
SunoopDogg <aswoo55555@gmail.com>
2024-04-16 01:00:38 +0900

    feat: Implement JWT-based user authentication

--- apps/auth/src/app/app.service.ts
+++ apps/auth/src/app/app.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { JwtPayloadDto } from './dto/jwt-payload.dto';
+import { UserDto } from './dto/user.dto';
+
+import { PrismaClientService } from '@ssampong-nest/prisma-client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaClientService,
+  ) {}
+
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<Omit<UserDto, 'password'>> {
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const { password, ...result } = user;
+      password;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: UserDto): Promise<{ accessToken: string }> {
+    const payload: JwtPayloadDto = {
+      email: user.email,
+      username: user.name,
+    };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
   }
 }
---
+++ apps/auth/src/app/dto/jwt-payload.dto.ts
@@ -0,0 +1,4 @@
+export class JwtPayloadDto {
+  email: string;
+  username: string;
+}
