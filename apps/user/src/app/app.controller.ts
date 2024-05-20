689b4f1547982e8d0d8413ab68f8f67d43254fcd
SunoopDogg <aswoo55555@gmail.com>
2024-05-20 09:48:10 +0900

    refactor: Remove unused user registration endpoint

--- apps/user/src/app/app.controller.ts
+++ apps/user/src/app/app.controller.ts
@@ -1,9 +1,6 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { CreateUserPayload, UserInterface } from './interfaces/user.interface';
-
-import { JwtAuthGuard, Role, Roles } from '@ssampong-nest/auth';
 
 @Controller()
 export class AppController {
@@ -12,12 +9,5 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
-  }
-
-  @Post('register')
-  @Roles(Role.ADMIN)
-  @UseGuards(JwtAuthGuard)
-  async register(@Body() payload: CreateUserPayload): Promise<UserInterface> {
-    return await this.appService.create(payload);
   }
 }
