3569f4b55d5f5fbb184e7274271c8451fa67a50f
SunoopDogg <aswoo55555@gmail.com>
2024-08-13 22:31:54 +0900

    feat: Add CRUD endpoints to gateway's AppController and AppService

--- apps/gateway/src/app/app.controller.ts
+++ apps/gateway/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -9,5 +9,25 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post()
+  postData() {
+    return this.appService.postData();
+  }
+
+  @Put()
+  putData() {
+    return this.appService.putData();
+  }
+
+  @Patch()
+  patchData() {
+    return this.appService.patchData();
+  }
+
+  @Delete()
+  deleteData() {
+    return this.appService.deleteData();
   }
 }
--- apps/gateway/src/app/app.service.ts
+++ apps/gateway/src/app/app.service.ts
@@ -5,4 +5,20 @@ export class AppService {
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
+
+  postData(): { message: string } {
+    return { message: 'Post API' };
+  }
+
+  putData(): { message: string } {
+    return { message: 'Put API' };
+  }
+
+  patchData(): { message: string } {
+    return { message: 'Patch API' };
+  }
+
+  deleteData(): { message: string } {
+    return { message: 'Delete API' };
+  }
 }
